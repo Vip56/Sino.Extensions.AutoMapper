45a2f268603f2709dc36d7721f82eb63bc793c09
姚正发 <976691141@qq.com>
2018-03-07 15:00:25 +0800

    init

---
+++ src/Sino.Extensions.AutoMapper/AutoMapper.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+
+namespace Sino.AutoMapper
+{
+    /// <summary>
+    /// IAutoMapper接口的实现
+    /// </summary>
+    public class AutoMapperImpl : IAutoMapper
+    {
+        private IMapper _mapper;
+
+        public AutoMapperImpl(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public TDestination Map<TDestination>(object source)
+        {
+            return _mapper.Map<TDestination>(source);
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source)
+        {
+            return _mapper.Map<TSource, TDestination>(source);
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+        {
+            return _mapper.Map<TSource, TDestination>(source, destination);
+        }
+    }
+}
---
+++ src/Sino.Extensions.AutoMapper/IAutoMapper.cs
@@ -0,0 +1,22 @@
+﻿using Sino.Dependency;
+
+namespace Sino.AutoMapper
+{
+    /// <summary>
+    /// 实体映射接口
+    /// </summary>
+    public interface IAutoMapper : ISingletonDependency
+    {
+        /// <summary>
+        /// 将source映射为指定类型
+        /// </summary>
+        /// <typeparam name="TDestination">需要映射成的类型</typeparam>
+        /// <param name="source">需要被映射的实体</param>
+        TDestination Map<TDestination>(object source);
+
+
+        TDestination Map<TSource, TDestination>(TSource source);
+
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+    }
+}
